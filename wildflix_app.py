17f66009a75b5fccdbc29d3b41db66887dda991e
Laura Vigé <93139216+LauraVige@users.noreply.github.com>
2022-03-22 13:21:34 +0100

    Remove print df

--- wildflix_app.py
+++ wildflix_app.py
@@ -19,16 +19,12 @@ uploaded_file = st.file_uploader("Import du fichier wildflix_project :")
 if uploaded_file is not None:
   df_mod_daw = pd.read_csv(uploaded_file, index_col="title")
 
-
 uploaded_file2 = st.file_uploader("Import du fichier wildflix__BDD_cine :")
 if uploaded_file2 is not None:
   df = pd.read_csv(uploaded_file2)
 
 # Variables numeriques a prendre en compte 
 X = df_mod_daw.select_dtypes('number')
-
-st.write(df.head())
-st.write(df_mod_daw.head())
 
 # Selection du film
 titre = st.text_input('Veuillez saisir un titre de film :', '')
