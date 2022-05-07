546dcc4c56fc1fbe02800a5bc14b40d7a510d14f
Laura Vigé <93139216+LauraVige@users.noreply.github.com>
2022-05-07 21:12:03 +0200

    Update wildflix_app2.py

--- wildflix_app2.py
+++ wildflix_app2.py
@@ -25,6 +25,7 @@ df_two = pd.read_csv("wildflix_project_two.csv")
 
 df_final = pd.concat([df_one,df_two])
 
+df_final.set_index("title", inplace=True)
 st.dataframe(df_final.head())
 
 # file with infos
